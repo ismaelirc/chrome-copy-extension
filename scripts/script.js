b084f3fb21438020d7760cfcc9eff4a916e7b507
Ismael Costa <ismael@Ismaels-MacBook-Pro.local>
2023-12-18 12:50:09 -0300

    Create Element init

--- scripts/script.js
+++ scripts/script.js
@@ -17,5 +17,29 @@ function addCopy(){
 }
 
 function saveToLocalStorage(objCopy){
+    console.log(objCopy);
+
+    const newElement = createElements('container_descriptions');
+    const elementCopy = createElements('copy_description', objCopy.text);
+    newElement.id = objCopy.id;
+    document.getElementById('container').appendChild(elementCopy);
+    let nE = newElement.querySelector(objCopy.id);
+
+    nE.appendChild(elementCopy);
+    document.getElementById('container').appendChild(newElement);
+    
+}
+
+function createElements(className,htmlText = '', type = 'div'){
+
+    const element = document.createElement(type);
+    element.classList.add(className);
+
+    if(htmlText){
+        element.appendChild(document.createTextNode(htmlText));
+
+    }
+
+    return element;
 
 }
